3d013040340eda717f4276d5c70b6bf22c33f206
abdelhakim telmoudy <telmoudy.abdelhakim@gmail.com>
2024-03-03 13:38:42 +0100

    first commit

---
+++ src/main/java/ext/DaoImplNWS.java
@@ -0,0 +1,12 @@
+package ext;
+
+
+import doa.IDao;
+
+public class DaoImplNWS  implements IDao {
+    @Override
+    public double getData() {
+        System.out.println("Version web services");
+        return 90;
+    }
+}
---
+++ src/main/java/ext/DoaImpl2.java
@@ -0,0 +1,16 @@
+package ext;
+
+
+import doa.IDao;
+import org.springframework.stereotype.Component;
+
+@Component("dao2")
+public class DoaImpl2 implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("version Capteurs");
+        double temp= 6000;
+        return temp;
+    }
+}
